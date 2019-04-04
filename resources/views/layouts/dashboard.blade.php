b7a4ef515dbcb007050a53e0f2889911915993c9
Rifqi Oktabhiar Erawan <36649810+RifqiErawan@users.noreply.github.com>
2019-04-04 15:24:39 +0700

    delete trash & temp update

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -2,14 +2,12 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
 
-  {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
   <title>Kostaria @yield('title')</title>
 
   <!-- Custom fonts for this template-->
@@ -17,15 +15,13 @@
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="{{ asset('dashboard/css/sb-admin-2.min.css')}}" rel="stylesheet">
+  <link href="{{asset('dashboard/css/sb-admin-2.min.css')}}" rel="stylesheet">
 
   <!-- Other Scripts -->
   @yield('stylesheets')
 
 </head>
-
 <body id="page-top">
-
   <!-- Page Wrapper -->
   <div id="wrapper">
 
@@ -212,7 +208,7 @@
 								<a class="dropdown-item" href="{{ route('kostariateam.logout') }}" data-toggle="modal" data-target="#logoutModal">
                   <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                   Logout
-                </a>                
+                </a>
 								@endauth
               </div>
             </li>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,92 +1,53 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Kostaria</title>
+@extends('layouts.app')
 
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/select2.min.css') }}" rel="stylesheet">
-    <!-- <link href="{{ asset('site-asset/css/modern-business.css') }}" rel="stylesheet"> -->
-  </head>
-<body>
-  <!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
-  <div class="container">
-    <a class="navbar-brand" href="#">
-          <img src="http://placehold.it/150x50?text=Logo" alt="">
-        </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home
-                <span class="sr-only">(current)</span>
-              </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Services</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+@section('stylesheets')
+<link href="{{ asset('css/select2.min.css') }}" rel="stylesheet">
+@endsection
 
-  <div class="container">
-
-    <form method="POST" action="{{ route('boardinghouses.search') }}">
-      @csrf
-      <div class="form-group">
+@section('content')
+<form method="POST" action="{{ route('boardinghouses.search') }}">
+    @csrf
+    <div class="form-group">
         <div class="hero__text">
-          <label for="id_label_single">
-            Pilih Kampus
-          </label>
-          <select class="select2 form-control" name="university">
-            <option>Pilih Kampus</option>
-          </select>
+            <label for="id_label_single">
+                Pilih Kampus
+            </label>
+            <select class="select2 form-control" name="university">
+                <option>Pilih Kampus</option>
+            </select>
         </div>
         <div class="">
-          <select class="select2 form-control" name="regency">
-            <option value="">Pilih Kota</option>
-          </select>
+            <select class="select2 form-control" name="regency">
+                <option value="">Pilih Kota</option>
+            </select>
         </div>
         <div class="col-sm-12 col-md-4">
-          <div class="form-group row mb-0">
-              <div class="col-md-12 pull-right">
-                  <button type="submit" class="btn btn-primary">
-                  {{ __('Search') }}
-                  </button>
-              </div>
-          </div>
+            <div class="form-group row mb-0">
+                <div class="col-md-12 pull-right">
+                    <button type="submit" class="btn btn-primary">
+                        {{ __('Search') }}
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </form>
+    </div>
+</form>
 
-    <h1 class="my-4">Welcome to Modern Business</h1>
-    <div class="row">
-      @if(isset($listBoardingHouse))
-      @foreach($listBoardingHouse as $boardinghouse)
-      <div class="col-lg-4 col-sm-6 portfolio-item">
+<h1 class="my-4">Welcome to Modern Business</h1>
+<div class="row">
+    @if(isset($listBoardingHouse))
+    @foreach($listBoardingHouse as $boardinghouse)
+    <div class="col-lg-4 col-sm-6 portfolio-item">
         <div class="card h-100">
-          <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-          <div class="card-body">
-            <h4 class="card-title">
-              <!-- <a href="#"></a> -->
-              <b>{{ $boardinghouse->name }}</b>
-            </h4>
-            <p class="card-text">{{$boardinghouse->address." ".$boardinghouse->village->name. ", ".$boardinghouse->village->district->name . ", " .  $boardinghouse->village->district->regency->name. ", " . $boardinghouse->village->district->regency->province->name}}</p>
-            <p class="card-text text-primary">{{$boardinghouse->information_cost}}</p>
-            <!-- <p>deskripsi : {{$boardinghouse->description}}</p>
+            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+            <div class="card-body">
+                <h4 class="card-title">
+                    <!-- <a href="#"></a> -->
+                    <b>{{ $boardinghouse->name }}</b>
+                </h4>
+                <p class="card-text">{{$boardinghouse->address." ".$boardinghouse->village->name. ", ".$boardinghouse->village->district->name . ", " .  $boardinghouse->village->district->regency->name. ", " . $boardinghouse->village->district->regency->province->name}}</p>
+                <p class="card-text text-primary">{{$boardinghouse->information_cost}}</p>
+                <!-- <p>deskripsi : {{$boardinghouse->description}}</p>
             <p>alamat : {{$boardinghouse->address." ".$boardinghouse->village->name. ", ".$boardinghouse->village->district->name . ", " .  $boardinghouse->village->district->regency->name. ", " . $boardinghouse->village->district->regency->province->name}}</p>
             @php
               $facilities = str_split($boardinghouse->facility);
@@ -105,17 +66,16 @@
             <p>akses : {{$boardinghouse->access}}</p>
             <p>informasi tambahan : {{$boardinghouse->information_others}}</p>
             <p>informasi biaya : {{$boardinghouse->information_cost}}</p> -->
-          </div>
-          <div class="card-footer">
-            <a href="#" class="btn btn-primary">Booking</a>
-            <a href="{{ route('boardinghouses.show', $boardinghouse->id)}}" class="btn btn-primary">See</a>
-          </div>
+            </div>
+            <div class="card-footer">
+                <a href="#" class="btn btn-primary">Booking</a>
+                <a href="{{ route('boardinghouses.show', $boardinghouse->id)}}" class="btn btn-primary">See</a>
+            </div>
         </div>
-      </div>
-      @endforeach
-      @endif
     </div>
-  </div>
+    @endforeach
+    @endif
+</div>
 
 <!-- <div class="container">
     <div class="row justify-content-center">
@@ -155,76 +115,76 @@
     </div>
 </div> -->
 
-</body>
-  <!-- Bootstrap core JavaScript -->
-  <script src="{{ asset('js/app.js') }}"></script>
-  <script src="{{ asset('js/select2.min.js') }}"></script>
-  <script type="text/javascript">
+@endsection
+@section('scripts')
+<script src="{{ asset('js/select2.min.js') }}"></script>
+<script type="text/javascript">
     $('.select2-single').select2();
-  </script>
-  <script type="text/javascript">
+</script>
+<script type="text/javascript">
     $(document).ready(function() {
-      $('select[name="university"]').select2({
-        ajax: {
-            url: '/universities/getUniversities',
-            dataType: 'json',
-            delay: 250,
-            data: function (params) {
-                return {
-                    q: params.term, // search term
-                };
+        $('select[name="university"]').select2({
+            ajax: {
+                url: '/universities/getUniversities',
+                dataType: 'json',
+                delay: 250,
+                data: function(params) {
+                    return {
+                        q: params.term, // search term
+                    };
+                },
+                processResults: function(data) {
+                    return {
+                        results: data
+                    };
+                },
+                cache: true
             },
-            processResults: function (data) {
-                return {
-                    results: data
-                };
+            minimumInputLength: 1,
+            placeholder: function() {
+                $(this).data('placeholder');
             },
-            cache: true
-        },
-        minimumInputLength: 1,
-        placeholder: function(){
-            $(this).data('placeholder');
-        },
 
-        templateResult: ResultTemplater,
-        templateSelection: SelectionTemplater
-    });
+            templateResult: ResultTemplater,
+            templateSelection: SelectionTemplater
+        });
 
-    $('select[name="regency"]').select2({
-        ajax: {
-            url: '/regencies/getRegencies',
-            dataType: 'json',
-            delay: 250,
-            data: function (params) {
-                return {
-                    q: params.term, // search term
-                };
+        $('select[name="regency"]').select2({
+            ajax: {
+                url: '/regencies/getRegencies',
+                dataType: 'json',
+                delay: 250,
+                data: function(params) {
+                    return {
+                        q: params.term, // search term
+                    };
+                },
+                processResults: function(data) {
+                    return {
+                        results: data
+                    };
+                },
+                cache: true
             },
-            processResults: function (data) {
-                return {
-                    results: data
-                };
+            minimumInputLength: 1,
+            placeholder: function() {
+                $(this).data('placeholder');
             },
-            cache: true
-        },
-        minimumInputLength: 1,
-        placeholder: function(){
-            $(this).data('placeholder');
-        },
-        templateResult: ResultTemplater,
-        templateSelection: SelectionTemplater
-    });
+            templateResult: ResultTemplater,
+            templateSelection: SelectionTemplater
+        });
 
-    function ResultTemplater(item) {
-        return item.name;
-    }
-    function SelectionTemplater(item) {
-        if(typeof item.name !== "undefined") {
-            return ResultTemplater(item);
+        function ResultTemplater(item) {
+            return item.name;
         }
-        return item.name;
-    }
+
+        function SelectionTemplater(item) {
+            if (typeof item.name !== "undefined") {
+                return ResultTemplater(item);
+            }
+            return item.name;
+        }
 
     });
-  </script>
-</html>
+</script>
+@endsection
