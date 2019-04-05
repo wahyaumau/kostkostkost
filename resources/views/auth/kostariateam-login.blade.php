395c737808e04034615073e479cb977f848a6167
wahyaumau <wahyaumau@gmail.com>
2019-04-05 17:45:49 +0700

    fixing login button in kostariateam-login

--- resources/views/auth/kostariateam-login.blade.php
+++ resources/views/auth/kostariateam-login.blade.php
@@ -18,22 +18,23 @@
               <div class="text-center">
                 <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
               </div>
-              <form class="user">
+              <form class="user" method="POST" action="{{ route('kostariateam.login.submit') }}">
+                @csrf
                 <div class="form-group">
-                  <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                  <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." name="email">
                 </div>
                 <div class="form-group">
-                  <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                  <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password" name="password">
                 </div>
                 <div class="form-group">
                   <div class="custom-control custom-checkbox small">
-                    <input type="checkbox" class="custom-control-input" id="customCheck">
+                    <input type="checkbox" class="custom-control-input" id="customCheck" name="remember">
                     <label class="custom-control-label" for="customCheck">Remember Me</label>
                   </div>
-                </div>
-                <a href="index.html" class="btn btn-primary btn-user btn-block">
-                  Login
-                </a>
+                </div>                
+                <button type="submit" class="btn btn-primary btn-user btn-block">
+                  {{ __('Login') }}
+                </button>
                 <hr>
                 <a href="index.html" class="btn btn-google btn-user btn-block">
                   <i class="fab fa-google fa-fw"></i> Login with Google
@@ -48,7 +49,7 @@
               </div>
               <div class="text-center">
                 <a class="small" href="register.html">Create an Account!</a>
-              </div>
+              </div>              
             </div>
           </div>
         </div>
